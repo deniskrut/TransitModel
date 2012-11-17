6c3eb10f8e767a452fbc264865b06a78e701310e
Denis Krut <denis.krut@gmail.com>
2012-11-18 00:39:01 +0200

    Initial Commit

---
+++ Public Transport Ukraine/PTViewController.h
@@ -0,0 +1,13 @@
+//
+//  PTViewController.h
+//  Public Transport Ukraine
+//
+//  Created by Denis Krut on 18.11.12.
+//  Copyright (c) 2012 Denis Krut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PTViewController : UIViewController
+
+@end
